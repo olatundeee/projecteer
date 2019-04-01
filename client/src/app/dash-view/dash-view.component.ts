3ebf5eae020bbb03a8a192416dc079ae6dac5e34
Olatunde Oladunni <wichitawebservices@gmail.com>
2019-04-01 20:25:17 +0100

    View all projects added by all registered users

--- client/src/app/dash-view/dash-view.component.ts
+++ client/src/app/dash-view/dash-view.component.ts
@@ -3,6 +3,7 @@ import { TasksService } from '../services/tasks.service';
 import { TeamsService } from '../services/teams.service';
 import { ProjectsService } from '../services/projects.service';
 import { Task } from '../task';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dash-view',
@@ -22,7 +23,7 @@ export class DashViewComponent implements OnInit {
   userteams;
 
   constructor(
-    private taskService: TasksService, private teamService: TeamsService, private projectService: ProjectsService
+    private taskService: TasksService, private teamService: TeamsService, private projectService: ProjectsService, private router: Router
   ) { }
 
   ngOnInit() {
@@ -89,4 +90,74 @@ export class DashViewComponent implements OnInit {
 
   }
 
+  // view details of all user projects
+
+
+  viewUserProject(userproject) {
+    // use local storage to store project data in order to view details;
+
+    localStorage.removeItem('project-name');
+    localStorage.setItem('project-name', userproject.project_name);
+
+    localStorage.removeItem('project-description');
+    localStorage.setItem('project-description', userproject.project_description);
+
+    localStorage.removeItem('project-solutions');
+    localStorage.setItem('project-solutions', userproject.project_solutions);
+
+    localStorage.removeItem('project-problems');
+    localStorage.setItem('project-problems', userproject.project_problems);
+
+    // use router to navigate to project details view
+
+    this.router.navigateByUrl('/dashboard/projects/project-details');
+  }
+
+  // view the tasks listed under the user's projects
+
+  viewUserProjectTasks(userproject) {
+    localStorage.removeItem('project-name');
+    localStorage.setItem('project-name', userproject.project_name);
+
+    // use router to navigate to project tasks view
+
+    this.router.navigateByUrl('/dashboard/tasks');
+  }
+
+  // retrieve details of one particular task for display
+
+  viewUserTask(usertask) {
+    // store task data in local storage for reference
+
+    localStorage.removeItem('task-id');
+    localStorage.setItem('task-id', usertask._id);
+
+    localStorage.removeItem('task-name');
+    localStorage.setItem('task-name', usertask.task_title);
+
+    localStorage.removeItem('task-description');
+    localStorage.setItem('task-description', usertask.task_description);
+
+    localStorage.removeItem('task-reason');
+    localStorage.setItem('task-reason', usertask.task_reason);
+
+    localStorage.removeItem('task-result');
+    localStorage.setItem('task-result', usertask.task_result);
+    this.router.navigateByUrl('/dashboard/tasks/task-detail');
+  }
+
+  // view details of the team
+
+  viewUserTeam(userteam) {
+    console.log(userteam);
+    // use local storage to store project data in order to assign data to team;
+
+    localStorage.removeItem('project-id');
+    localStorage.setItem('project-id', userteam.team_projectId);
+
+    localStorage.removeItem('project-name');
+    localStorage.setItem('project-name', userteam.team_project);
+
+    this.router.navigateByUrl('/dashboard/teams');
+  }
 }
