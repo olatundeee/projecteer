f42a7837309126d0c322e8f542db066d9b4fb9cd
Olatunde Oladunni <wichitawebservices@gmail.com>
2019-04-08 21:33:33 +0100

    if a user has updated their profile the add bio button will not be displayed

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -10,14 +10,25 @@ import { Router } from '@angular/router';
 export class ProfileComponent implements OnInit {
 
   profile;
+  displayAddBio;
 
   constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+    // display add bio button on initialization
+
+    this.displayAddBio = true;
+
     // get profile details for the currently logged in user
 
     this.userService.getProfileDetails().subscribe(res => {
       this.profile = res;
+
+      // check if user has already added a profile, if true, do not display the add bio button
+
+      if (this.profile.profile_added === true) {
+        this.displayAddBio = false;
+      }
     });
   }
 
